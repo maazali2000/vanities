9e346e412a10ba2b2d847cd1e3a4ffed5cce0cd8
maazali2000 <maaz.ali@genesisengr.com>
2024-03-18 15:33:02 -0400

    Added some content in vanities furniture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FieldsetModule } from 'primeng/fieldset';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MyCommonModule } from './common/common.module';
 import { VanitiesFurnitureComponent } from './components/vanities-furniture/vanities-furniture.component';
 import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +18,9 @@ import { CardModule } from 'primeng/card';
     BrowserModule,
     AppRoutingModule,
     MyCommonModule,
-    CardModule
+    CardModule,
+    FieldsetModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
